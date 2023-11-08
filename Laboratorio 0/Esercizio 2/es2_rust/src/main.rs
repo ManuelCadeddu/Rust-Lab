5fcb1defa0d7e0cab637e2152c3930e4ffd02ce3
ManuelCadeddu <100230575+ManuelCadeddu@users.noreply.github.com>
2023-11-08 13:57:50 +0100

    Lab0_es2_update
    Sono stati eliminati i warning ed è stato implementato il tratto Display

--- Laboratorio 0/Esercizio 2/es2_rust/src/main.rs
+++ Laboratorio 0/Esercizio 2/es2_rust/src/main.rs
@@ -1,44 +1,58 @@
+use std::fmt;
 use std::fs::{File};
 use std::io::Read;
 use clap::{Arg, Command};
 
 #[derive(Debug)]
-
 enum DataStruct {
-
-    ValueStruct { data_type: i32, val: f32, timestamp: i32 },     
+    ValueStruct { data_type: i32, val: f32, timestamp: i32 },
     MValueStruct { data_type: i32, val: [f32; 10], timestamp: i32 },
-    MessageStruct { data_type: i32, message: [char; 21] }
+    MessageStruct { data_type: i32, message: [char; 21] },
 }
 
 struct CData {
-
     data_type: i32,
     values: DataStruct,
 }
 
+impl fmt::Display for CData {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match &self.values {
+            DataStruct::ValueStruct { data_type, val, timestamp } => {
+                write!(f, "[ValueStruct ({})] Data Type: {}, Value: {}, Timestamp: {}", &self.data_type, data_type, val, timestamp)
+            }
+            DataStruct::MValueStruct { data_type, val, timestamp } => {
+                write!(f, "[MValueStruct ({})] Data Type: {}, Value: [{}, {}, {}, {}, {}, {}, {}, {}, {}, {}], Timestamp: {}", &self.data_type, data_type, val[0], val[1], val[2], val[3], val[4], val[5], val[6], val[7], val[8], val[9], timestamp)
+            }
+            DataStruct::MessageStruct { data_type, message } => {
+                let message_string: String = message.iter().collect();
+                write!(f, "[MessageStruct ({})] Data Type: {}, Message: {}", &self.data_type, data_type, message_string)
+            }
+        }
+    }
+}
+
 impl CData {
-
     fn from_file(file: &mut File) -> CData {
-
         let mut buffer: [u8; 52] = [0; 52];
 
+        // La funzione 'expect' serve per gestire gli errori in modo esplicito quando si lavora con Result o Option
         file.read_exact(&mut buffer).expect("Error reading from file");
-       
+
+        // Il tratto TryInto consente la conversione tra tipi di dato. Per farlo si usa il metodo try_into, che rende un Result
         let data_type = i32::from_le_bytes((&buffer[4..8]).try_into().unwrap());
 
         let values = match data_type {
-
             1 => {
                 DataStruct::ValueStruct {
-                    data_type: data_type,
+                    data_type,
                     val: f32::from_le_bytes((&buffer[8..12]).try_into().unwrap()),
                     timestamp: i32::from_le_bytes((&buffer[12..16]).try_into().unwrap()),
                 }
             }
             2 => {
                 DataStruct::MValueStruct {
-                    data_type: data_type,
+                    data_type,
                     val: {
                         let mut val = [0.0; 10];
                         for i in 0..10 {
@@ -51,7 +65,7 @@ impl CData {
             }
             3 => {
                 DataStruct::MessageStruct {
-                    data_type: data_type,
+                    data_type,
                     message: {
                         let mut message = ['\0'; 21];
                         for i in 0..21 {
@@ -69,7 +83,7 @@ impl CData {
 }
 
 fn main() {
-    
+
     // Verifica dei parametri da linea di comando
     let matches = Command::new("MyApp")
         .version("1.0")
@@ -77,18 +91,18 @@ fn main() {
         .about("This app takes values from a C legacy program in order to process data using a new Rust functionality")
         .arg(
             Arg::new("input")
-            .short('i')
-            .long("input")
-            .value_name("FILE")
-            .help("Sets the input file to use")
-            .required(true),
+                .short('i')
+                .long("input")
+                .value_name("FILE")
+                .help("Sets the input file to use")
+                .required(true),
         )
         .get_matches();
 
-    if let Some(input) = matches.get_one::<String>("input"){
+    if let Some(input) = matches.get_one::<String>("input") {
 
         // Apertura del file
-       let mut file = File::open(input)   // File::open rende un Result<File, io::Error>
+        let mut file = File::open(input)   // File::open rende un Result<File, io::Error>
             .unwrap();
 
         // Metodo alternativo per la gestione degli errori
@@ -101,12 +115,12 @@ fn main() {
         // Lettura del file
         let mut c_data: Vec<CData> = Vec::new();
 
-        for _i in 0..100 {  
+        for _i in 0..100 {
             c_data.push(CData::from_file(&mut file));
         }
 
         for val in c_data {
-            println!("{:?}", val.values);
+            println!("{}", val);
         }
     }
 }
---
+++ Laboratorio 0/Esercizio 2/es2_rust/target/debug/deps/clap-a88533bea56c3319.d
@@ -0,0 +1,9 @@
+C:\Users\manuc\Desktop\Manuel\Poli\Programmazione di Sistema\rust-labs\Rust-Lab\Laboratorio 0\Esercizio 2\es2_rust\target\debug\deps\clap-a88533bea56c3319.rmeta: C:\Users\manuc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.4.7\src\lib.rs C:\Users\manuc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.4.7\src\../examples/demo.rs C:\Users\manuc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.4.7\src\../examples/demo.md
+
+C:\Users\manuc\Desktop\Manuel\Poli\Programmazione di Sistema\rust-labs\Rust-Lab\Laboratorio 0\Esercizio 2\es2_rust\target\debug\deps\libclap-a88533bea56c3319.rlib: C:\Users\manuc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.4.7\src\lib.rs C:\Users\manuc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.4.7\src\../examples/demo.rs C:\Users\manuc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.4.7\src\../examples/demo.md
+
+C:\Users\manuc\Desktop\Manuel\Poli\Programmazione di Sistema\rust-labs\Rust-Lab\Laboratorio 0\Esercizio 2\es2_rust\target\debug\deps\clap-a88533bea56c3319.d: C:\Users\manuc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.4.7\src\lib.rs C:\Users\manuc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.4.7\src\../examples/demo.rs C:\Users\manuc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.4.7\src\../examples/demo.md
+
+C:\Users\manuc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.4.7\src\lib.rs:
+C:\Users\manuc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.4.7\src\../examples/demo.rs:
+C:\Users\manuc\.cargo\registry\src\index.crates.io-6f17d22bba15001f\clap-4.4.7\src\../examples/demo.md:
